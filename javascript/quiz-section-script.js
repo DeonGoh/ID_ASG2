50645c6eed833603ff28e3551b33f7fe950f96c0
Your own username <Your own email>
2023-02-10 18:02:03 +0800

    big push

--- javascript/quiz-section-script.js
+++ javascript/quiz-section-script.js
@@ -21,20 +21,22 @@ $(document).ready(function () {
           $.ajax(settings).done(function (response) {
             console.log(response);
             $('#test').append(`<h1>${subject}</h1>`);
-
+                
             for(var i = 0; i < response.length; i++){
                 if(response[i]["QuizCat"] == subject){
                     
                     $('#test').append(`
-                    <div class="quiz-block">
-                        <div class="image-container">
-                            <img src="${response[i].Image}">
-                        </div>
-                        <div class="text-container">
-                            <h2>${response[i].QuizName}</h2>
-                            <p>${response[i].QuizDesc}</p>
-                        </div>
-                    </div>`)
+                    <div class="item">
+                            <a href="#">
+                                <div class="card">
+                                    <img src="${response[i].Image}" alt="...">
+                                    <div class="card-body">
+                                        <h5 class="card-title text-center">${response[i].QuizName}</h5>
+                                        <p class="card-text">${response[i].QuizDesc}</p>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>`)
                 }
             }
         });
